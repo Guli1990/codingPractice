a2281802fe421fb842d1b69855ce9e26e425a992
Gulgina Abdusalam <gulgina@Nijats-MacBook-Pro.local>
2021-04-18 14:11:49 -0400

    solved string2 zipzap

--- src/test/java/stringproblems/String2ZipZap.java
+++ src/test/java/stringproblems/String2ZipZap.java
@@ -8,6 +8,19 @@ public class String2ZipZap {
     zipZap("zzzopzop") → "zzzpzp"
      */
     public String zipZap(String str) {
-        return null;
+        String result = "";
+
+        for(int i=0; i < str.length(); i++){
+            //extract single letter and add them to the result
+            result += str.substring(i,i+1);
+
+            if(i>0 && i<str.length()-1){
+                if(str.charAt(i-1)=='z' && str.charAt(i+1)=='p'){
+                    result = result.substring(0, result.length()-1);
+                }
+
+            }
+        }
+        return result;
     }
 }
